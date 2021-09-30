795c9c312aa05ec2eef4534095cbe12c161a8f54
zeeshan-shabbir <zeeshanshabbir186@gmail.com>
2021-09-30 20:56:24 +0500

    using context start

--- src/Components/ShowProduct/ShowProduct.js
+++ src/Components/ShowProduct/ShowProduct.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect,useContext } from 'react'
+import { GlobalContext } from '../../context/context'
 import { Link, useLocation, useHistory, useParams } from "react-router-dom";
 import Data from "../../Assets/Data/Data"
 import Card from '../Card/Card'
